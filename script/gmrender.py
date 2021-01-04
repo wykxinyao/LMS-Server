5f291d7ffd06e5b4cdf48f1b8b69ecc1e4f0000a
wykxinyao <604114726@qq.com>
2021-01-04 15:10:57 +0800

    增加了gmrender和shairport的脚本

---
+++ script/gmrender.py
@@ -0,0 +1,89 @@
+# coding:utf8
+# author:Lysio
+# python version:2.7
+
+"""
+    Dietpi Config Server Copyright 2004-2020 Dreamtech.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    You may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+import os
+import utils.service_util as service
+
+
+def check_status():
+    """
+    检查gmrender服务开启状态
+    :return:
+    """
+    result = service.check_status("gmrender")
+    if result == "active":
+        return True
+    elif result == "inactive":
+        return False
+    else:
+        return False
+
+
+def start_service():
+    """
+    启动服务
+    :return: None
+    """
+    service.start("gmrender")
+
+
+def stop_service():
+    """
+    关闭服务
+    :return: None
+    """
+    service.stop("gmrender")
+
+
+def restart_service():
+    """
+    重启服务
+    :return: None
+    """
+    service.restart("gmrender")
+
+
+def boot_status():
+    """
+    开机启动状态
+    :return:
+    """
+    result = service.boot_status("gmrender")
+    if result:
+        return True
+    else:
+        return False
+
+
+def boot_start():
+    """
+    设置服务开机启动
+    :return: None
+    """
+    service.boot_start("gmrender")
+
+
+def boot_stop():
+    """
+    设置服务开机不启动
+    :return:None
+    """
+    service.boot_stop("gmrender")
+
